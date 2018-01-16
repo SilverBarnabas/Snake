265efe51dc8110df76ef44cb88df05ce7bda2c98
SilverBarnabas <silverbarnabas@hot.ee>
2018-01-16 09:34:16 +0200

    Initial commit

---
+++ Snake/Food.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+
+namespace Snake
+{
+    class Food
+    {
+        public Rectangle Piece;
+        private int x, y, width = 10, height = 10;
+
+        public Food(Random rand)
+        {
+
+        }
+        public void Draw(Graphics graphics)
+        {
+
+        }
+        public void Generate(Random rand)
+        {
+            x = rand.Next(0, 30) * 10;
+            y = rand.Next(0, 20) * 10;
+        }
+    }
+}
---
+++ Snake/frmMain.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Snake
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMenu = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelMenu
+            // 
+            this.labelMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.labelMenu.Location = new System.Drawing.Point(0, 0);
+            this.labelMenu.Name = "labelMenu";
+            this.labelMenu.Size = new System.Drawing.Size(300, 200);
+            this.labelMenu.TabIndex = 0;
+            this.labelMenu.Text = "Snake Game\r\nSilver \r\nta\r\n";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 200);
+            this.Controls.Add(this.labelMenu);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(316, 239);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(316, 239);
+            this.Name = "frmMain";
+            this.Text = "Snake - Score:1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMenu;
+    }
+}
+
